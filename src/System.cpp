4f74130c9aafe5050db269af0a2770ea2427d465
renierminnaar <renierminnaar@proton.me>
2022-09-15 10:20:14 -0600

    initial commit

---
+++ src/System.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Renier Minnaar on 2022-09-14.
+//
+
+#include "System.h"
---
+++ src/System.h
@@ -0,0 +1,16 @@
+//
+// Created by Renier Minnaar on 2022-09-14.
+//
+// This is the main loop for the program and
+// controls all other processes
+//
+
+
+#pragma once
+
+
+class System {
+public:
+    System();
+    
+};
